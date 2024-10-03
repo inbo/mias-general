2525e824d16345d9b3875f36d1c52b66941227f7
Janne Adolf <janne.adolf@inbo.be>
2024-10-03 16:56:23 +0200

    document download_table_prius()

--- source/functions/download_table_prius.R
+++ source/functions/download_table_prius.R
@@ -1,6 +1,21 @@
 download_table_prius <- function(
     path = "data/raw"
 ) {
+  #' Download the PrIUS table from Zenodo
+  #'
+  #' @description \code{download_table_prius} applies the function \code{\link[inborutils]{download_zenodo}} from the \href{https://inbo.github.io/inborutils/}{inborutils} package to download the table from the PrIUS report from Zenodo (\href{https://zenodo.org/records/7678524}{website}). Of the files available on Zenodo only the .csv file is written to the provided path.
+  #'
+  #' @param path A character string holding a path name
+  #'
+  #' @examples
+  #'
+  #' \dontrun{
+  #' download_table_prius(
+  #'   path = "data/raw"
+  #' )
+  #' }
+  #'
+  #' @export
   inborutils::download_zenodo(
     doi = "10.5281/zenodo.7678524",
     path = path
