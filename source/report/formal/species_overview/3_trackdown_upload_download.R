1721d2976a0f5d520bc5ed2e1351d2f98b38c96f
Janne Adolf <janne.adolf@inbo.be>
2024-10-15 12:00:24 +0200

    add update functionality to trackdown script

--- source/report/formal/species_overview/3_trackdown_upload_download.R
+++ source/report/formal/species_overview/3_trackdown_upload_download.R
@@ -7,34 +7,44 @@ if (FALSE) googledrive::drive_auth() # make sure to tick all boxes
 if (FALSE) googledrive::drive_user() # check user
 #
 #
-# ---upload to g-drive------------------------------------------------------
+# ---upload to / update on g-drive-------------------------------------------
 #
-# upload section rmd files (files already uploaded are skipped)
+# file list
 files_list <- list.files(
   path = "source/report/formal/species_overview/sections",
   full.names = TRUE
-  )
+)|>
+  grep(pattern = "skeleton", invert = TRUE, value = TRUE)
+#
+# upload or update?
+which_fun <- "trackdown::update_file" # upload_file OR update_file
+#
+# upload/update section rmd files (files already uploaded are skipped)
 for (i in seq_along(files_list)){
   file_path_i <- files_list[i]
   try(
-    trackdown::upload_file(
-    file = file_path_i,
-    gpath = "trackdown/species_overview/sections",
-    shared_drive = "PRJ_meetnetten-IUS"
-    )
+    do.call(
+      what =   eval(parse(text = which_fun)),
+      args = list(
+        file = file_path_i,
+        gpath = "trackdown/species_overview/sections",
+        shared_drive = "PRJ_meetnetten-IUS"
+      ))
   )
 }
 #
-# upload abstract rmd file
+# upload/update abstract rmd file
 try(
-  trackdown::upload_file(
-    file = "source/report/formal/species_overview/abstract/abstract.Rmd",
-    gpath = "trackdown/species_overview",
-    shared_drive = "PRJ_meetnetten-IUS"
-  )
+  do.call(
+    what =   eval(parse(text = which_fun)),
+    args = list(
+      file = "source/report/formal/species_overview/abstract/abstract.Rmd",
+      gpath = "trackdown/species_overview",
+      shared_drive = "PRJ_meetnetten-IUS"
+    ))
 )
 #
-# upload pdf final report
+# upload/update pdf final report
 # folder ID is in URL
 try(
   googledrive::drive_upload(
@@ -43,16 +53,9 @@ try(
   )
 )
 #
-#
 # ---download from g-drive -------------------------------------------------
 #
 if (FALSE) {
   trackdown::download_file()
 }
 #
-#
-# ---update on g-drive------------------------------------------------------
-#
-if (FALSE) {
-  trackdown::update_file()
-}
