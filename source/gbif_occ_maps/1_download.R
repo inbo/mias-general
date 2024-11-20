f3f973bf89f2373cad896e6da72eaf92601d9dfc
Janne Adolf <janne.adolf@inbo.be>
2024-11-20 16:16:58 +0100

    download for flanders + buffer regions

--- source/gbif_occ_maps/1_download.R
+++ source/gbif_occ_maps/1_download.R
@@ -2,10 +2,25 @@ rm(list = ls())
 #
 # -----------------------------------------------------------------------------
 #
+# define polygon flanders + buffer
+buffer <- 1000*100 # in meters
+fla_borders <- sf::st_read("data/gis/prius/flanders_wgs84.geojson") # flanders
+fla_borders_buffer <- sf::st_buffer(x = fla_borders, dist = buffer)
+plot_check <- ggplot2::ggplot() + # plot to check
+  ggplot2::geom_sf(data = fla_borders_buffer, fill = "blue", size = 0.2) +
+  ggplot2::geom_sf(data = fla_borders, fill = "coral", size = 0.2)
+fla_borders_buffer_txt <- fla_borders_buffer |>
+  sf::st_geometry() |>
+  sf::st_as_text() |>
+  wk::wkt() |>
+  wk::wk_orient()
+# CHECK: https://docs.ropensci.org/rgbif/articles/getting_occurrence_data.html
+#
 # define gbif parameters
 scientific_names <- c("Muntiacus reevesi (Ogilby, 1839)", "Faxonius virilis (Hagen, 1870)") # not used
-country <- "BE"
-area_id <- "BEL.2_1" # flanders
+country <- "BE" # not used
+area_id <- "BEL.2_1" # flanders # not used
+# https://docs.ropensci.org/rgbif/articles/getting_occurrence_data.html
 basis_of_record <- c(
   "OBSERVATION",
   "HUMAN_OBSERVATION",
@@ -22,8 +37,9 @@ hasCoordinate <- TRUE
 # download
 gbif_download_args <- list(
   #rgbif::pred_in("scientificName", scientific_names),
-  rgbif::pred("country", country),
-  rgbif::pred("gadm", area_id),
+  #rgbif::pred("country", country),
+  #rgbif::pred("gadm", area_id),
+  rgbif::pred_within(fla_borders_buffer_txt),
   rgbif::pred_in("basisOfRecord", basis_of_record),
   rgbif::pred_gte("year", year_begin),
   rgbif::pred_lte("year", year_end),
