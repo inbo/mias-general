b3cd7e9fb83ddfb1678114ad0643453d449352ac
Janne Adolf <janne.adolf@inbo.be>
2024-11-26 12:11:52 +0100

    update script to write google script to get view urls

--- source/functions/create_appsscript_writeviewurl.R
+++ source/functions/create_appsscript_writeviewurl.R
@@ -1,21 +1,30 @@
 create_appsscript_writeviewurl <- function(
-    form_id,
+    form_ids,
     gdrive_destfolder_id,
-    name_outtxtfile
+    name_outfile
 ){
-  if (!grepl("\\.txt", name_outtxtfile)){
-    name_outtxtfile <- paste0(name_outtxtfile, ".txt")
-  }
-  form_script <- paste(
+  paste(
     "function writeFormUrlToFile() {",
-    sprintf("  var formId = '%s';", form_id),
-    sprintf("  var folderId = '%s';", gdrive_destfolder_id),
-    sprintf("  var fileName = '%s';", name_outtxtfile),
-    "  var form = FormApp.openById(formId);",
-    "  var viewUrl = form.getPublishedUrl();",
-    "  var content = viewUrl;",
-    "  var folder = DriveApp.getFolderById(folderId);",
-    "  var file = folder.createFile(fileName, content);",
+    sprintf(
+    "var formIdList = (['%s']);",
+    paste(form_ids, collapse = "','")
+    ),
+    sprintf("var folderId = '%s';", gdrive_destfolder_id),
+    sprintf("var fileName = '%s';", name_outfile),
+    "var fileContent = [['formid', 'formtitle', 'viewurl']];",
+    "for (var i = 0; i < formIdList.length; i++) {",
+    "var formId = formIdList[i];",
+    "var form = FormApp.openById(formId);",
+    "var formTitle = DriveApp.getFileById(formId).getName();",
+    "var viewUrl = form.getPublishedUrl();",
+    "fileContent[i+1] = [formId, formTitle, viewUrl];",
+    "}",
+    "var folder = DriveApp.getFolderById(folderId);",
+    "var spreadsheet = SpreadsheetApp.create(fileName);",
+    "var file = DriveApp.getFileById(spreadsheet.getId());",
+    "file.moveTo(folder);",
+    "var sheet = spreadsheet.getSheets()[0];",
+    "sheet.getRange(1, 1, fileContent.length, fileContent[0].length).setValues(fileContent);",
     "}",
     NULL,
     sep = "\n"
