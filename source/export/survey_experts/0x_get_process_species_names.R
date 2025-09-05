a16e8f9cd48cb7462107f9b82fd146cd79f1ef8d
Janne Adolf <janne.adolf@inbo.be>
2025-09-05 15:19:28 +0200

    update union list status

--- source/export/survey_experts/0x_get_process_species_names.R
+++ source/export/survey_experts/0x_get_process_species_names.R
@@ -8,7 +8,7 @@ list.files("source/functions", full.names = TRUE) |>
 # --- get species list from survey expert sheet ---------------
 #
 # from file "Prius_data_modified_extended.gsheet"
-# g-drive path: PRJ_MIUS\_overkoepelend\data
+# g-drive path: PRJ_MIUS\_overkoepelend\data\species
 #
 species_sheetid_args <- list(
   sheet_id = "1sd-AXrETsRI01XIry5YU2V8Vl-_UU9y2CwgM3y01fTk",
@@ -44,7 +44,11 @@ species_upd_tmp1 <- species |>
 #
 species_upd_tmp2 <- species_upd_tmp1 |>
   # 1) indicator for union list
-  dplyr::rename(tidyselect::any_of(c(on_unionlist = "unielijst"))) |>
+  dplyr::rename(
+    tidyselect::any_of(
+      c(on_unionlist = "unielijst", on_unionlist_2025 = "unielijst_2025")
+      )
+    ) |>
   dplyr::mutate(dplyr::across(dplyr::contains("on_unionlist"), as.logical)) |>
   # 2) status in Flanders according to PrIUS report
   dplyr::rename(tidyselect::any_of(
@@ -58,7 +62,8 @@ species_upd_tmp2 <- species_upd_tmp1 |>
 vern_names <- lapply(
   species_upd_tmp2$key_gbif_acc,
   function(x){
-    rgbif::name_usage(key = x, data = "vernacularNames") |>
+    tmp <- try(
+      rgbif::name_usage(key = x, data = "vernacularNames") |>
       purrr::pluck("data") |>
       dplyr::filter(grepl("eng|nld|fra|deu", language)) |>
       dplyr::distinct(language, .keep_all = TRUE) |>
@@ -67,7 +72,17 @@ vern_names <- lapply(
         names_from = language,
         values_from = vernacularName,
         names_prefix = "vern_name_gbif_"
+      ),
+      silent = TRUE
+    )
+    out <- if (tmp |> inherits(x = _, "try-error")){
+      data.frame(
+        "taxonKey" = x
       )
+    } else {
+      tmp
+    }
+    return(out)
   }) |>
   dplyr::bind_rows()
 #
@@ -128,9 +143,18 @@ knotweed_vernnames_nld <- if (TRUE) {
 }
 #
 # 3) misgurnus mohoity (Dybowski, 1869):
-# add vernacular name in English
+# add vernacular name in Dutch and English
 misgurnus_vernname_eng <- "Oriental wheatherfish"
+misgurnus_vernname_nld <- "Noord-aziatische modderkruiper"
 #
+# 4) Ondatra zibethicus (Linnaeus, 1766):
+# replace vernacular name in English, which is German
+ondatra_vernname_eng <- "Muskrat"
+#
+# 5) Pachycondyla chinensis (Emery, 1895)
+# add vernacular name in Dutch
+pachycondyla_vernname_nld <- "Aziatische Naaldmier"
+
 # adapt
 species_upd <- species_upd_tmp3 |>
   dplyr::mutate(
@@ -146,10 +170,13 @@ species_upd <- species_upd_tmp3 |>
     vern_name_gbif_eng_alt = dplyr::case_when(
       grepl("Misgurnus mohoity", sci_name_gbif_acc) ~ misgurnus_vernname_eng,
       grepl("Reynoutria", sci_name_gbif_acc) ~ knotweed_vernnames_eng,
+      grepl("Ondatra zibethicus", sci_name_gbif_acc) ~ ondatra_vernname_eng,
       TRUE ~ NA_character_
     ),
     vern_name_gbif_nld_alt = dplyr::case_when(
+      grepl("Misgurnus mohoity", sci_name_gbif_acc) ~ misgurnus_vernname_nld,
       grepl("Reynoutria", sci_name_gbif_acc) ~ knotweed_vernnames_nld,
+      grepl("Pachycondyla", sci_name_gbif_acc) ~ pachycondyla_vernname_nld,
       TRUE ~ NA_character_
     )
   ) |>
@@ -170,6 +197,7 @@ species_list <- list(
   meta = data.frame(
     variablename = c(
       "on_unionlist",
+      "on_unionlist_2025",
       "kingdom",
       "taxon",
       "prius_stadium",
@@ -191,7 +219,8 @@ species_list <- list(
       "vern_name_gbif_deu"
     ),
     content = c(
-      "whether the species is currently (see date) on the union list",
+      "whether the species is on the union list in 2022",
+      "whether the species is on the union list in 2025",
       "kingdom of species",
       "taxon of species",
       "stadium in Flanders according to prius report",
@@ -240,3 +269,86 @@ googledrive::drive_mv(
   file = tmp_id,
   path = "https://drive.google.com/drive/folders/1m0dzMZlIyvHe5CEIyly7Ps47NUT8oRSH" |> paste0(x = _, "/")
 )
+#
+#
+#
+# --- compare 2 species lists ---------------
+#
+list_1 <- get(load(paste0("data/processed/2025-04-30_species_list.Rda")))
+list_2 <- get(load(paste0("data/processed/2025-09-04_species_list.Rda")))
+data_1 <- list_1 |> purrr::pluck("data") |> dplyr::arrange(sci_name_gbif_acc)
+data_2 <- list_2 |> purrr::pluck("data") |> dplyr::arrange(sci_name_gbif_acc) |> dplyr::select(- "on_unionlist_2025")
+
+
+with_width <- function(width, code) {
+  withr::local_options(width = width)
+  code
+}
+test <- lapply(
+  seq_along(data_1),
+  \(i){
+    waldo::compare(
+      data_1[, i] ,
+      data_2[, i],
+      max_diffs = nrow(data_1)
+    ) |> with_width(
+      width = 100,
+      code = _
+    )
+  }
+)
+
+test[[7]] # sci_name_gbif
+test[[15]] # vern_name_gbif_nld
+test[[16]] # vern_name_gbif_nld_alt
+test[[17]] # vern_name_gbif_eng
+test[[18]] # vern_name_gbif_eng_alt
+#test[[19]] # vern_name_gbif_fra
+#test[[20]] # vern_name_gbif_deu
+#
+#
+#
+# --- only update unionlist 2025 column ---------------
+#
+species_old_upd_tmp <- data_1 |>
+  dplyr::full_join(
+    x = _,
+    y = list_2 |> purrr::pluck("data") |> dplyr::select(c("sci_name_gbif_acc", "on_unionlist_2025"))
+  )
+
+# reduced manual adaptions
+# 3) misgurnus mohoity (Dybowski, 1869):
+# add vernacular name in Dutch
+misgurnus_vernname_nld <- "Noord-aziatische modderkruiper"
+#
+# 4) Ondatra zibethicus (Linnaeus, 1766):
+# replace vernacular name in English, which is German
+ondatra_vernname_eng <- "Muskrat"
+#
+# 5) Pachycondyla chinensis (Emery, 1895)
+# add vernacular name in Dutch
+pachycondyla_vernname_nld <- "Aziatische Naaldmier"
+
+# adapt
+species_old_upd <- species_old_upd_tmp |>
+  dplyr::mutate(
+    vern_name_gbif_eng_alt = dplyr::case_when(
+      grepl("Ondatra zibethicus", sci_name_gbif_acc) ~ ondatra_vernname_eng,
+      TRUE ~ vern_name_gbif_eng_alt
+    ),
+    vern_name_gbif_nld_alt = dplyr::case_when(
+      grepl("Misgurnus mohoity", sci_name_gbif_acc) ~ misgurnus_vernname_nld,
+      grepl("Pachycondyla", sci_name_gbif_acc) ~ pachycondyla_vernname_nld,
+      TRUE ~ vern_name_gbif_nld_alt
+    )
+  )
+
+# compare
+waldo::compare(data_1, species_old_upd)
+
+# save
+species_list <- list(
+  data = species_old_upd,
+  meta = list_2$meta
+)
+save(species_list, file = paste0("data/processed/2025-04-30_species_list_upd.Rda"))
